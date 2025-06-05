e922c93db3b41e61f2ace22710c91977c01a807f
AnThony69x <157445810+AnThony69x@users.noreply.github.com>
2025-06-05 08:24:58 -0500

    Agregar configuración del servidor y proxy en vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'https://gestiontareas-252537812380.europe-west1.run.app/api',
+        changeOrigin: true,
+      }
+    }
+  },
 })
